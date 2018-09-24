3415a916aba511af4053e252f33293a7c70d9aae
Marseglia <lumarseglia@deloitte.com>
2018-09-24 15:24:50 -0700

    Initial commit

--- app.js
+++ app.js
@@ -139,64 +139,7 @@ function getMovieDetail(userId, field) {
     }
   });
 }
-/*
-function findMovie(userId, movieTitle) {
-  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
-    if (!error &amp;&amp; response.statusCode === 200) {
-      var movieObj = JSON.parse(body);
-      if (movieObj.Response === "True") {
-        var query = {user_id: userId};
-        var update = {
-          user_id: userId,
-          title: movieObj.Title,
-          plot: movieObj.Plot,
-          date: movieObj.Released,
-          runtime: movieObj.Runtime,
-          director: movieObj.Director,
-          cast: movieObj.Actors,
-          rating: movieObj.imdbRating,
-          poster_url:movieObj.Poster
-        };
-        var options = {upsert: true};
-        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-          if (err) {
-            console.log("Database error: " + err);
-          } else {
-            message = {
-              attachment: {
-                type: "template",
-                payload: {
-                  template_type: "generic",
-                  elements: [{
-                    title: movieObj.Title,
-                    subtitle: "Is this the movie you are looking for?",
-                    image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
-                    buttons: [{
-                      type: "postback",
-                      title: "Yes",
-                      payload: "Correct"
-                    }, {
-                      type: "postback",
-                      title: "No",
-                      payload: "Incorrect"
-                    }]
-                  }]
-                }
-              }
-            };
-            sendMessage(userId, message);
-          }
-        });
-      } else {
-          console.log(movieObj.Error);
-          sendMessage(userId, {text: movieObj.Error});
-      }
-    } else {
-      sendMessage(userId, {text: "Something went wrong. Try again."});
-    }
-  });
-}
-*/
+
 function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
@@ -228,4 +171,63 @@ function processPostback(event) {
   } else if (payload === "Incorrect") {
     sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
   }
+}
+
+
+function findMovie(userId, movieTitle) {
+  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
+    if (!error &amp;&amp; response.statusCode === 200) {
+      var movieObj = JSON.parse(body);
+      if (movieObj.Response === "True") {
+        var query = {user_id: userId};
+        var update = {
+          user_id: userId,
+          title: movieObj.Title,
+          plot: movieObj.Plot,
+          date: movieObj.Released,
+          runtime: movieObj.Runtime,
+          director: movieObj.Director,
+          cast: movieObj.Actors,
+          rating: movieObj.imdbRating,
+          poster_url:movieObj.Poster
+        };
+        var options = {upsert: true};
+        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+          if (err) {
+            console.log("Database error: " + err);
+          } else {
+            message = {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: movieObj.Title,
+                    subtitle: "Is this the movie you are looking for?",
+                    image_url: movieObj.Poster === "N/A?http://placehold.it/350x150" : movieObj.Poster,
+                    //image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                    buttons: [{
+                      type: "postback",
+                      title: "Yes",
+                      payload: "Correct"
+                    }, {
+                      type: "postback",
+                      title: "No",
+                      payload: "Incorrect"
+                    }]
+                  }]
+                }
+              }
+            };
+            sendMessage(userId, message);
+          }
+        });
+      } else {
+          console.log(movieObj.Error);
+          sendMessage(userId, {text: movieObj.Error});
+      }
+    } else {
+      sendMessage(userId, {text: "Something went wrong. Try again."});
+    }
+  });
 }

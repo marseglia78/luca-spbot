5b1e1e521670c965405f5a42a31a7f6dbd408eb9
Marseglia <lumarseglia@deloitte.com>
2018-09-24 18:16:48 -0700

    Initial commit

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ function getMovieDetail(userId, field) {
 function findMovie(userId, movieTitle) {
     //site="http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle
     //request("http://www.omdbapi.com/?t="+movieTitle+"&amp;apikey=9ff32eb9",
-    site="http://www.omdbapi.com/?t=rocky&amp;apikey=9ff32eb9"
+    site="http://www.omdbapi.com/?t=rocky&amp;plot=full&amp;apikey=9ff32eb9"
     var decoded = site.replace(/&amp;/g, '&');
     request(decoded,
     function (error, response, body) {

35a6453347e627b719deffdfc7c0bac5cb42f354
Marseglia <lumarseglia@deloitte.com>
2018-09-24 18:14:29 -0700

    Initial commit

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ function findMovie(userId, movieTitle) {
           rating: movieObj.imdbRating,
           poster_url:movieObj.Poster
         };
-        sendMessage(userId, {text: plot});
+        //sendMessage(userId, {text: plot});
         /*
         var options = {upsert: true};
         Movie.findOneAndUpdate(query, update, options, function(err, mov) {

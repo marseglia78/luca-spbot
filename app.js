7eef5ed2ee5c401edcea46446e9a6f7cc431a309
Marseglia <lumarseglia@deloitte.com>
2018-09-24 17:58:09 -0700

    Initial commit

--- app.js
+++ app.js
@@ -215,10 +215,11 @@ function findMovie(userId, movieTitle) {
     request(decoded,
     function (error, response, body) {
     //if (!error && response.statusCode === 200)
-    sendMessage(userId, {text: response});
+    sendMessage(userId, {text: "I am in"});
     if (!error) {
       var movieObj = JSON.parse(body);
       if (movieObj.Response === "True") {
+        sendMessage(userId, {text: movieObj.Plot});
         var query = {user_id: userId};
         var update = {
           user_id: userId,
